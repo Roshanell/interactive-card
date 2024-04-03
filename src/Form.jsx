432812ce504199d29cdf270bd83d1eeb73b0c765
Roshanell <96213223+Roshanell@users.noreply.github.com>
2024-04-03 11:28:59 -0500

    adjusts width on forms

--- src/Form.jsx
+++ src/Form.jsx
@@ -10,7 +10,7 @@ const Form = ({
 }) => {
   return (
     <form className="text-black">
-      <div className="absolute right-60 top-10 form-controls">
+      <div className="absolute right-32 top-40 w-1/3">
         <div className="mb-4 form-control">
           <label className="label">
             <span className="font-bold label-text">Cardholder Name</span>
@@ -42,55 +42,57 @@ const Form = ({
             maxLength={16}
           />
         </div>
-        <div className="mb-4 form-control">
-          <label className="label">
-            <span className="font-bold label-text">Month</span>
-          </label>
-          <input
-            type="text"
-            placeholder="Type here"
-            className="input border-2 border-slate-200"
-            required
-            name="expirationMonth"
-            id="expirationMonth"
-            value={formData.expirationMonth}
-            onChange={handleChange}
-            maxLength={2}
-          />
-        </div>
-        <div className="mb-4 form-control">
-          <label className="label">
-            <span className="font-bold label-text">Year</span>
-          </label>
-          <input
-            type="text"
-            placeholder="Type here"
-            className="input border-2 border-slate-200"
-            required
-            name="expirationYear"
-            id="expirationYear"
-            value={formData.expirationYear}
-            onChange={handleChange}
-            maxLength={4}
-          />
-        </div>
-        <div className="mb-4 form-control">
-          <label className="label">
-            <span className="font-bold label-text">CVV</span>
-          </label>
-          <input
-            type="text"
-            maxLength={3}
-            placeholder="Type here"
-            className="input border-2 border-slate-200"
-            name="cvv"
-            id="cvv"
-            value={formData.cvv}
-            onChange={handleChange}
-          />
+        <div className="flex flex-wrap">
+          <div className="mb-4 w-1/3">
+            <label className="label">
+              <span className="font-bold label-text">Month</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input border-2 border-slate-200 w-28"
+              required
+              name="expirationMonth"
+              id="expirationMonth"
+              value={formData.expirationMonth}
+              onChange={handleChange}
+              maxLength={2}
+            />
+          </div>
+          <div className="mb-4 w-1/3 ">
+            <label className="label">
+              <span className="font-bold label-text">Year</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input border-2 border-slate-200 w-28"
+              required
+              name="expirationYear"
+              id="expirationYear"
+              value={formData.expirationYear}
+              onChange={handleChange}
+              maxLength={4}
+            />
+          </div>
+          <div className="mb-4 w-1/3">
+            <label className="label">
+              <span className="font-bold label-text">CVV</span>
+            </label>
+            <input
+              type="text"
+              maxLength={3}
+              placeholder="Type here"
+              className="input border-2 border-slate-200 w-36"
+              name="cvv"
+              id="cvv"
+              value={formData.cvv}
+              onChange={handleChange}
+            />
+          </div>
           <button
             type="submit"
-            className="btn mt-4"
+            className="btn mt-4 w-full"
             onClick={currentStep !== 3 ? handleNextStep : handleFormReset}
           >
             Confirm
