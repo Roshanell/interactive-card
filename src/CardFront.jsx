d19e97c30531af67e809b4dc168bc77037cd18d3
Roshanell <96213223+Roshanell@users.noreply.github.com>
2024-04-03 10:50:57 -0500

    Positions card number

--- src/CardFront.jsx
+++ src/CardFront.jsx
@@ -3,18 +3,20 @@ import "./App.css";
 
 const CardFront = ({ formData }) => {
   return (
-    <div className="absolute top-14 left-24 card-container">
-      <div className="absolute left-10 top-10 w-12 h-12 rounded-full bg-white large-circle"></div>
-      <div className="absolute left-24 top-10 w-6 h-6 rounded-full border border-white transparent-circle"></div>
+    <div className="absolute top-14 left-24">
+      {/* circles */}
+      <div className="absolute left-10 top-10 w-12 h-12 rounded-full bg-white"></div>
+      <div className="absolute left-24 top-10 w-6 h-6 rounded-full border border-white"></div>
       <img src={require("../src/images/bg-card-front.png")} alt="" />
-      <div className="absolute left-10 top-80 name">{formData.name}</div>
+      {/* card info */}
+      <div className="absolute left-10 top-52">{formData.name}</div>
       <div
-        className="absolute left-10 top-50 card-number"
+        className="absolute left-10 top-36 card-number"
         style={{ letterSpacing: "5px" }}
       >
         {formData.cardNumber}
       </div>
-      <div className="absolute right-15 top-80 expiration-month">
+      <div className="absolute right-15 top-80">
         {formData.expirationMonth}/
       </div>
       <div className="absolute right-5 top-80 expiration-year">
