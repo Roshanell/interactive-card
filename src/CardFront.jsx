8512c54ae9635894e5dfb5fc729756ebf6f20195
Roshanell <96213223+Roshanell@users.noreply.github.com>
2024-04-03 10:42:50 -0500

    adjusts circles to be in propoer position

--- src/CardFront.jsx
+++ src/CardFront.jsx
@@ -4,8 +4,8 @@ import "./App.css";
 const CardFront = ({ formData }) => {
   return (
     <div className="absolute top-14 left-24 card-container">
-      <div className="absolute left-10 top-20 w-12 h-12 rounded-full bg-white large-circle"></div>
-      <div className="absolute left-25 top-20 w-6 h-6 rounded-full bg-blue-400 border border-white transparent-circle"></div>
+      <div className="absolute left-10 top-10 w-12 h-12 rounded-full bg-white large-circle"></div>
+      <div className="absolute left-24 top-10 w-6 h-6 rounded-full border border-white transparent-circle"></div>
       <img src={require("../src/images/bg-card-front.png")} alt="" />
       <div className="absolute left-10 top-80 name">{formData.name}</div>
       <div
