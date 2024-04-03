157335ae1ea8517fcd803904bca7e3d46e7a823a
Roshanell <96213223+Roshanell@users.noreply.github.com>
2024-04-03 11:04:20 -0500

    positions cvv on card back  on desktop

--- src/CardBack.jsx
+++ src/CardBack.jsx
@@ -4,7 +4,7 @@ const CardBack = ({ formData }) => {
   return (
     <div className="absolute left-52 bottom-4 card-back-container">
       <img src={require("../src/images/bg-card-back.png")} alt="" />
-      <div className="absolute right-10 top-45 cvv">{formData.cvv}</div>
+      <div className="absolute right-11 top-28 cvv">{formData.cvv}</div>
     </div>
   );
 };
